d41edde6f7f22ee9b6edaa0de491158c98563204
Alexandros Papasopoulos <al.pap@live.com>
2022-06-29 02:55:56 +0200

    added components

---
+++ packages/src/components/Switch.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let inline: boolean = false
+  export let disabled: boolean = false
+  export let label: string = ''
+  export let name: string = ''
+  let id = (Math.random() + 1).toString(36)
+</script>
+
+<div {...$$restProps} class={`custom-switch ${$$restProps.class ? $$restProps.class : ''}`} class:d-inline-block={inline}>
+  <input type="checkbox" {disabled} on:click {id} {name} />
+  <label for={id}>{label}</label>
+</div>
---
+++ packages/src/components/SwitchGroup.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Switch from './Switch.svelte'
+  export let inline: boolean = false
+  export let disabled: boolean = false
+  export let name: string = ''
+  export let space: string = ''
+  export let items: { label: ''; value: '' }[] = []
+  export let selected: string[] = []
+  export let single: boolean = false
+</script>
+
+<div {...$$restProps}>
+  {#each items as item}
+    <Switch
+      class={`${space ? 'm-' + space : ''}`}
+      {inline}
+      {disabled}
+      {name}
+      label={item.label}
+      on:click={() => {
+        if (single && selected.includes(item.value)) selected.filter((i) => i !== item.value)
+        selected.push(item.value)
+      }}
+    />
+  {/each}
+</div>
